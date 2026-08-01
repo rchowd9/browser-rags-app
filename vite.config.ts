a761c3671b84374d6062e3f5e7d611988b84ad0c
rchowd9 <chowdhuryriasat078@gmail.com>
2026-07-31 22:28:30 -0400

    added dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // GitHub Pages deploys under subpaths: https://username.github.io/repo-name/
-  base: process.env.NODE_ENV === 'production' ? '/<your-repo-name>/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/browser-rags-app/' : '/',
   worker: {
     format: 'es'
   }
